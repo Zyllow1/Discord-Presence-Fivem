0d7437ad69be89a0c564c32d602301d1d5f19915
Zyllow <124933975+Zyllow1@users.noreply.github.com>
2024-02-20 15:32:18 +0100

    Add files via upload

---
+++ discord_presence/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zyllow'
+
+shared_script 'shared/config.lua'
+server_script 'server/server.lua'
+client_script 'client/client.lua'
---
+++ discord_presence/shared/config.lua
@@ -0,0 +1,14 @@
+Config = {
+    DiscordAppID = 1152918741986840606,
+    AppBigAssetID = 'orizon2',
+    AppBigAssetText = 'Orizon',
+    AppSmallAssetID = 'orizonn2',
+    AppSmallAssetText = 'Roleplay',
+    FirstButtonPlaceholder = 'Discord',
+    FirstButtonLink = 'https://discord.gg/orizon',
+    SecondButtonPlaceholder = 'Site Internet',
+    SecondButtonLink = 'https://discord.gg/orizon',
+
+    UpdateEvery = 10000,
+    Template = 'ID: {ID} | Nom: {NAME} | {PLAYERS_COUNT}'
+}
